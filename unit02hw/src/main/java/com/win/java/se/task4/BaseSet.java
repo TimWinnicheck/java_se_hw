47ec1e08f83a1428831f15c6d50a18c01e2d42d6
TimWinnicheck <vinnichekt@yandex.ru>
2017-02-26 16:38:36 +0300

    Task4 - Implemented sortbyprice

--- unit02hw/src/main/java/com/win/java/se/task4/BaseSet.java
+++ unit02hw/src/main/java/com/win/java/se/task4/BaseSet.java
@@ -1,5 +1,7 @@
 package main.java.com.win.java.se.task4;
 
+import java.util.Arrays;
+
 public class BaseSet implements ComparisonInterface{
 
     Implement[] Contents;
@@ -29,7 +31,29 @@ public class BaseSet implements ComparisonInterface{
 
     public void sortbyprice(){
 
+        Implement[] temp = new Implement[this.Contents.length];
+        System.arraycopy(Contents,0,temp,0,this.Contents.length);
 
+        int i = 0;
+        int j = 0;
+        Implement tmp;
+
+
+        while (i < amount){
+            j = 0;
+            while (j < amount-1){
+                if (temp[j].cost < temp[j+1].cost)
+                {
+                    tmp = temp[j+1];
+                    temp[j+1] = temp[j];
+                    temp[j] = tmp;
+                }
+                j++;
+            }
+            i++;
+        }
+
+        this.Contents = temp;
 
     }
 
--- unit02hw/src/test/java/com/win/java/se/test4/BaseSetTest.java
+++ unit02hw/src/test/java/com/win/java/se/test4/BaseSetTest.java
@@ -1,9 +1,38 @@
 package test.java.com.win.java.se.test4;
 
-import main.java.com.win.java.se.task3.*;
+import main.java.com.win.java.se.task4.*;
 import org.junit.Test;
 
 public class BaseSetTest {
+
+    @Test
+    public void sortbypricetest() throws Exception {
+
+        Eraser eraser = new Eraser();
+        Pen pen = new Pen();
+        Pencil pencil = new Pencil();
+        Ruler ruler = new Ruler();
+
+        BaseSet set = new BaseSet();
+
+        set.add(pencil);
+        set.add(pen);
+        set.add(eraser);
+        set.add(ruler);
+
+        set.list();
+
+        set.sortbyprice();
+
+        set.list();
+
+    }
+
+    @Test
+    public void sortbyname() throws Exception {
+
+    }
+
     @Test
     public void addtest() throws Exception {
 
