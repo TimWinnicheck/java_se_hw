b0b0ef8fbc9fba50912e5f998d775ae8be8ef19e
TimWinnicheck <vinnichekt@yandex.ru>
2017-02-26 13:59:54 +0300

    added equals() hashcode() and tostring(0

--- unit02hw/src/main/java/com/win/java/se/Pen.java
+++ unit02hw/src/main/java/com/win/java/se/Pen.java
@@ -22,4 +22,33 @@ public class Pen {
         this.Length = Length;
         this.Manufacturer = "Unknown";
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+
+        Pen pen = (Pen) other;
+
+        if (Length != pen.Length) return false;
+        if (!Color.equals(pen.Color)) return false;
+        return Manufacturer.equals(pen.Manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Color.hashCode();
+        result = 31 * result + Length;
+        result = 31 * result + Manufacturer.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Pen{" +
+                "Color='" + Color + '\'' +
+                ", Length=" + Length +
+                ", Manufacturer='" + Manufacturer + '\'' +
+                '}';
+    }
 }
