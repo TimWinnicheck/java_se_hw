c2ef02de5c9d92b332d628cdb4a1a1c757375c4c
TimWinnicheck <vinnichekt@yandex.ru>
2017-02-26 13:48:12 +0300

    Add files via upload

---
+++ unit02hw/src/main/java/com/win/java/se/Pen.java
@@ -0,0 +1,18 @@
+package main.java.com.win.java.se;
+
+public class Pen {
+
+    private char[] Color;
+    private int Length;
+    private char[] Manufacturer;
+
+    public Pen() {
+    }
+
+    public Pen(char[] Color, int Length, char[] Manufacturer)
+    {
+        this.Color = Color;
+        this.Length = Length;
+        this.Manufacturer = Manufacturer;
+    }
+}
