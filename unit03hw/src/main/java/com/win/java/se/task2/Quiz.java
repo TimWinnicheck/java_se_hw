1bd9666ace769d7b23d76aade549def75a05ff41
TimWinnicheck <vinnichekt@yandex.ru>
2017-03-05 17:08:21 +0300

    Task 3-2 Finished

--- unit03hw/src/main/java/com/win/java/se/task2/Quiz.java
+++ unit03hw/src/main/java/com/win/java/se/task2/Quiz.java
@@ -5,15 +5,28 @@ import java.util.Locale;
 public class Quiz {
 
     Locale locale;
+    ResourceProperty Bundle;
+    int questions = 3;
 
     Quiz (Locale locale) {
         this.locale = locale;
+        this.Bundle = new ResourceProperty(locale);
     }
 
     public String answer(int number) {
-        ResourceProperty Bundle = new ResourceProperty(this.locale);
         return Bundle.getValue(String.valueOf(number));
 
+    }
+
+
+
+    public void startQuiz() {
+        int i = 1;
+        while (i <= 3)
+        {
+            System.out.println(i+". : "+Bundle.getValue("q" + String.valueOf(i)));
+            i++;
+        }
     }
 
 }
--- unit03hw/src/test/java/com/win/java/se/task2/QuizTest.java
+++ unit03hw/src/test/java/com/win/java/se/task2/QuizTest.java
@@ -7,6 +7,18 @@ import java.util.Locale;
 import static org.junit.Assert.*;
 
 public class QuizTest {
+
+    @Test
+    public void PrintQuestionsTest() throws Exception {
+        Locale en = new Locale("en", "US");
+        Locale ru = new Locale("ru", "RU");
+
+        Quiz RUQuiz = new Quiz(ru);
+        Quiz ENQuiz = new Quiz(en);
+
+        RUQuiz.startQuiz();
+        ENQuiz.startQuiz();
+    }
     @Test
     public void answertest() throws Exception {
 
