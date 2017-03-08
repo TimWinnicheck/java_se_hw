4a61d557941073c2dc743a721342b1539d21e152
TimWinnicheck <vinnichekt@yandex.ru>
2017-03-08 11:45:24 +0300

    Task 3-3 Initial Commit

---
+++ unit03hw/src/main/java/com/win/java/se/task3/Reader.java
@@ -0,0 +1,16 @@
+package com.win.java.se.task3;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Reader {
+
+    private FileInputStream FIStream;
+
+
+    public Reader() throws FileNotFoundException{
+
+    }
+
+}
+
