c5cc49de5121f75a820520cd428da4668d30c943
TimWinnicheck <vinnichekt@yandex.ru>
2017-03-08 11:51:37 +0300

    Task 3-3 Initial Constructor

--- unit03hw/src/main/java/com/win/java/se/task3/Reader.java
+++ unit03hw/src/main/java/com/win/java/se/task3/Reader.java
@@ -1,16 +1,23 @@
 package com.win.java.se.task3;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class Reader {
 
+    private String pathname;
     private FileInputStream FIStream;
 
 
-    public Reader() throws FileNotFoundException{
-
+    public Reader(String pathname) throws FileNotFoundException{
+        File file = new File(pathname);
+        this.pathname = pathname;
+        if (!file.exists() && file.isDirectory()) throw new FileNotFoundException();
     }
+
+
+
 
 }
 
