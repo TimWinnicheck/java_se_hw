49d9628d57bff651c9c570736327e3a390cc801f
TimWinnicheck <vinnichekt@yandex.ru>
2017-03-13 15:25:31 +0300

    Task5-1 Started, added base methods

---
+++ unit05hw/src/main/java/com/win/java/se/task1/CommandLine.java
@@ -0,0 +1,26 @@
+package com.win.java.se.task1;
+
+import java.util.Scanner;
+
+public class CommandLine {
+
+    public static void Start() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            String currentCommand = scanner.nextLine();
+
+            CommandLine.coordinator(currentCommand);
+
+        }
+    }
+
+    private static void coordinator(String currentCommand) {
+
+        String[] splitCommand = currentCommand.split(" ");
+
+    }
+
+}
