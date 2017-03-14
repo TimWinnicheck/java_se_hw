c059393b0874c830995026e286600a042ee2a6a3
TimWinnicheck <vinnichekt@yandex.ru>
2017-03-14 12:47:54 +0300

    Task 5-2 adding tests

---
+++ unit05hw/src/test/java/com/win/java/se/task2/PropertiesManagerTest.java
@@ -0,0 +1,15 @@
+package com.win.java.se.task2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PropertiesManagerTest {
+    @Test
+    public void readingTest() throws Exception {
+
+        PropertiesManager.read("resorces/source1.properties", )
+
+    }
+
+}
