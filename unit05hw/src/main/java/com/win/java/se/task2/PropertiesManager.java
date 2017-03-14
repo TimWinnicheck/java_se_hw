031dc27e60e5a975e495169c5a16a79e212c318b
TimWinnicheck <vinnichekt@yandex.ru>
2017-03-14 12:38:27 +0300

    Task 5-2 added checkforexisting and read methods

--- unit05hw/src/main/java/com/win/java/se/task2/PropertiesManager.java
+++ unit05hw/src/main/java/com/win/java/se/task2/PropertiesManager.java
@@ -1,16 +1,39 @@
 package com.win.java.se.task2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 public class PropertiesManager {
 
-    public static String read(String path) {
+    public static String read(String path) throws IOException {
 
         Path file = Paths.get(path);
 
         checkForExisting(file, path);
+
+        StringBuilder SB = new StringBuilder();
+        Properties properties = new Properties();
+        InputStream IS = new FileInputStream(path);
+
+        properties.load(IS);
+        for (int i = 0; i < Values.values().length; i++) {
+            if (properties.containsKey(Values.values()[i].getIdentifier()))
+                SB.append(Values.values()[i].getIdentifier() + ": " + properties.getProperty(Values.values()[i].getIdentifier()) + "\n");
+            else
+                try {
+                    throw new IllegalKeyException(Values.values()[i].getIdentifier());
+                } catch (IllegalKeyException e) {
+                    e.printStackTrace();
+                }
+
+        }
+        return SB.toString();
 
 
     }
