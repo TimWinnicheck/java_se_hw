9d84df084a825fd35741d820a814a9f04d63e45f
TimWinnicheck <vinnichekt@yandex.ru>
2017-03-14 12:52:57 +0300

    Task 5-2 Finished

--- unit05hw/src/test/java/com/win/java/se/task2/PropertiesManagerTest.java
+++ unit05hw/src/test/java/com/win/java/se/task2/PropertiesManagerTest.java
@@ -2,13 +2,29 @@ package com.win.java.se.task2;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class PropertiesManagerTest {
     @Test
     public void readingTest() throws Exception {
 
-        PropertiesManager.read("resorces/source1.properties", )
+        System.out.println(PropertiesManager.read("resources/source1.properties"));
+
+    }
+
+    @Test(expected = IllegalPropertiesFileException.class)
+    public void noFileExceptionTest() throws IOException {
+
+        System.out.println(PropertiesManager.read("no.properties"));
+
+    }
+
+    @Test(expected = IllegalKeyException.class)
+    public void illegalKeyExceptionTest() throws IOException {
+
+        System.out.println(PropertiesManager.read("resources/source2.properties"));
 
     }
 
