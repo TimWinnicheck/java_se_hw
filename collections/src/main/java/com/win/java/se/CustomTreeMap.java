77b25670ea98d16fc3a20d1b90c27a3219e84ecc
TimWinnicheck <vinnichekt@yandex.ru>
2017-04-09 07:54:03 +0300

    TreeMap - added size

---
+++ collections/src/main/java/com/win/java/se/CustomTreeMap.java
@@ -0,0 +1,125 @@
+package com.win.java.se;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class CustomTreeMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+    private Node<K, V> root;
+    private int size = 0;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        Objects.requireNonNull(key);
+
+        if (root == null) return false;
+        root.key.compareTo((K) key);
+        return find(root, (K) key) != null;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        if (root == null) return false;
+        if (root.value == null) {
+            return value == null;
+        } else {
+            return root.value.equals(value);
+        }
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        Objects.requireNonNull(key);
+        root = put(root, key, value);
+        return value;
+    }
+
+    private Node<K, V> put(Node<K, V> node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node<>(key, value);
+        }
+        if (node.key.equals(key)) {
+            node.value = value;
+        } else if (node.key.compareTo(key) > 0) {
+            node.left = put(node.left, key, value);
+        } else {
+            node.right = put(node.right, key, value);
+        }
+        return node;
+    }
+
+    private Node<K, V> find(Node<K, V> node, K key) {
+        if (node == null) {
+            return null;
+        }
+        if (node.key.equals(key)) {
+            return node;
+        } else if (node.key.compareTo(key) > 0) {
+            return find(node.left, key);
+        } else {
+            return find(node.right, key);
+        }
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+
+    private class Node<K extends Comparable<K>, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> left;
+        private Node<K, V> right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+
+}
---
+++ collections/src/test/java/com/win/java/se/CustomTreeMapTest.java
@@ -0,0 +1,138 @@
+package com.win.java.se;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@FixMethodOrder
+public class CustomTreeMapTest {
+
+    private Map<Integer, String> m;
+
+    @Before
+    public void init() {
+        m = new CustomTreeMap<>();
+    }
+
+    @Test
+    public void testThatWeCanCreate() {
+
+        assertThat(m, is(notNullValue()));
+
+    }
+
+    @Test
+    public void testThatNewMapIsEmpty() {
+        assertThat(m.isEmpty(), is(true));
+    }
+
+    @Test
+    public void testThatOnNewMapContainKeyMethodReturnFalseForAnyObject() {
+        assertThat(m.containsKey(new Integer(1)), is(false));
+    }
+
+    @Test
+    public void testThatWeCanPutKeyValuePairAndCanCheckIt() {
+        m.put(new Integer(3), "abc");
+        assertThat(m.containsKey(3), is(true));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThatWeCantPutNullKey() {
+        m.put(null, "abc");
+    }
+
+    @Test
+    public void testThatWeCanPutNullValue() {
+        m.put(1, null);
+        assertThat(m.containsKey(1), is(true));
+    }
+
+    @Test
+    public void testThatMapCanPutPairWithKeyThatAlreadyPresented() {
+
+        String oldValue = "aaaa";
+        String newValue = "bbbb";
+
+        m.put(1, oldValue);
+        m.put(1, newValue);
+
+        assertFalse(m.containsValue(oldValue));
+        assertTrue(m.containsValue(newValue));
+    }
+
+    @Test
+    public void testThatIfWePutNewValueOnExistingKeyPreviousValueWillBeReturned() {
+        String oldValue = "aaaa";
+        String newValue = "bbbb";
+
+        m.put(1, oldValue);
+        String returnedValue = m.put(1, newValue);
+
+        assertThat(oldValue, is(equalTo(returnedValue)));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThatContainsKeyMethodThrowsExceptionOnNullKey() {
+        m.containsKey(null);
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void testThatContainsKeyMethodThrowsExceptionOnWrongKeyClass() {
+        m.put(1, ""); //TODO need to remove
+        m.containsKey(new String(""));
+    }
+
+    @Test
+    public void testContainsValueMethodWorksProperlyOn() {
+        String value = "aaaa";
+
+        m.put(1, value);
+
+        assertTrue(m.containsValue(value));
+    }
+
+    @Test
+    public void testContainsValueMethodWorksProperlyOnNullInputValue() {
+        String value = "aaaa";
+
+        m.put(1, value);
+
+        assertFalse(m.containsValue(null));
+    }
+
+    @Test
+    public void testThatWeCanPut10DifferentKeysInMap() {
+        IntStream.range(1, 10).forEach(
+                i -> m.put(i, String.valueOf(i))
+        );
+
+        IntStream.range(1, 10).forEach(
+                i -> assertTrue(m.containsKey(i))
+        );
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void testValueContainsMethodThrowsExceptionOnWrongInputValueClass() {
+    }
+
+    @Test
+    public void testThatMapCalculateItsSizeProperly() {
+        m.put(1, "fff");
+        m.put(2, "xxx");
+        m.put(3, "dddd");
+        assertTrue(m.size() == 3);
+    }
+
+
+}
